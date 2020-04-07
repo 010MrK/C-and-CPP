21d3e176ebd7abe3026e87babccc23f3d65e5a5e
k <nomail>
2020-04-07 22:18:46 +0300

    go

--- Teme/Tema 4/Tema4.cpp
+++ Teme/Tema 4/Tema4.cpp
@@ -6,25 +6,64 @@
 using namespace std;
 
 void FunctieAfisare(int[], int);
-void CitireCifre(int[], int&);
+void CitireCifreInputUnu(int[], int&);
+void CitireCifreInputDoi(int[], int&);
 void SumaElementelorUnuiVector(int[], int);         //P1
 void CautareaElementuluiMinim(int[], int);          //P2
 void OrdineaCrescatoareSauDesc(int[], int);         //P3
+void OrdonareCrescatoareSortareLiniara(int[], int); //P4
+void OrdonareCrescatoareSortareBubble(int[], int); //P5
 
 int main() {
     int x[200];
-    int n;
-    CitireCifre(x, n);
+    int y[200];
+    int nX;
+    int nY;
+    CitireCifreInputUnu(x, nX);
+    CitireCifreInputDoi(y, nY);
     // SumaElementelorUnuiVector(x, n);
     // CautareaElementuluiMinim(x, n);
     // OrdineaCrescatoareSauDesc(x, n);
+    // OrdonareCrescatoareSortareLiniara(x, n);
+    // OrdonareCrescatoareSortareBubble(x, n);
+}
+
+void OrdonareCrescatoareSortareBubble(int x[], int n) {
+    int flag = 0;
+    do
+    {
+        flag = 0;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (x[i] > x[i + 1])
+            {
+                swap(x[i], x[i + 1]);
+                flag = 1;
+            }
+
+        }
+
+    } while (flag == 1);
+    FunctieAfisare(x, n);
+}
+
+void OrdonareCrescatoareSortareLiniara(int x[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (x[i] > x[j]) {
+                swap(x[i], x[j]);
+            }
+        }
+    }
+    FunctieAfisare(x, n);
 }
 
 void OrdineaCrescatoareSauDesc(int x[], int n) {
     bool ordineC = false;// = true;
     bool ordineD = false;
     for (int i = 0; i < n - 1; i++) {
-        if (x[i] < x[i + 1]) 
+        if (x[i] < x[i + 1])
             ordineC = true;
         else {
             ordineC = false;
@@ -34,7 +73,7 @@ void OrdineaCrescatoareSauDesc(int x[], int n) {
 
     if (ordineC == false) {
         for (int i = 0; i < n - 1; i++) {
-            if (x[i] > x[i + 1]) 
+            if (x[i] > x[i + 1])
                 ordineD = true;
             else {
                 ordineD = false;
@@ -45,14 +84,14 @@ void OrdineaCrescatoareSauDesc(int x[], int n) {
 
     if (ordineC == false && ordineD == false) {
         cout << " Numerele nu sunt in ordine" << endl;
-    } 
-    else if (ordineC == true) 
+    }
+    else if (ordineC == true)
         cout << "Numerele sunt in ordine crescatoare" << endl;
     else if (ordineD == true)
         cout << "Numerele sunt in ordine descrescatoare" << endl;
 }
-    
-        
+
+
 
 void CautareaElementuluiMinim(int x[], int n) {
     int elementulMinim = INT_MAX;
@@ -72,8 +111,17 @@ void SumaElementelorUnuiVector(int x[], int n) {
     cout << "Suma elementelor din vector este: " << suma;
 }
 
-void CitireCifre(int x[], int&n) {
+void CitireCifreInputUnu(int x[], int&n) {
     ifstream read("Input.txt");
+    read >> n;
+    for (int i = 0; i < n; i++) {
+        read >> x[i];
+    }
+    FunctieAfisare(x, n);
+}
+
+void CitireCifreInputDoi(int x[], int&n) {
+    ifstream read("Input2.txt");
     read >> n;
     for (int i = 0; i < n; i++) {
         read >> x[i];

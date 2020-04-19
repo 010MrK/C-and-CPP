f0ce0f865c7cf94abb9d594d5cc731a328da6b74
k <nomail>
2020-04-19 16:32:05 +0300

    last update

---
+++ RecursivitateaNFactorial/RecursivitateaNFactorial.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int);
+
+int main() 
+{
+    cout << factorial(5) << " ";
+}
+int factorial(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * factorial(n - 1);
+    }
+}
--- Teme/Tema 4/Tema4.cpp
+++ Teme/Tema 4/Tema4.cpp
@@ -7,24 +7,25 @@ using namespace std;
 ifstream read("Input.txt");
 
 void FunctieAfisare(int[], int);
-void CitireCifreInputUnu(int[], int&);
-void CitireCifreInputDoi(int[], int&);
-void SumaElementelorUnuiVector(int[], int);                         //P1
-void CautareaElementuluiMinim(int[], int);                          //P2
-void OrdineaCrescatoareSauDesc(int[], int);                         //P3
-void OrdonareCrescatoareSortareLiniara(int[], int);                 //P4
-void OrdonareCrescatoareSortareBubble(int[], int);                  //P4
-void SumaSiDiferentaADoiVectori(int[], int, int[], int, int[]);     //P5
-void InterclasareVectori(int[], int, int[], int, int[]);            //P6
-void StergereElementDePePozitiaK(int[], int&, int);                 //P7
-void InserareElementPePozitiaK(int[], int&, int, int);              //P8
+void CitireCifreInputUnu(int[], int &);
+void CitireCifreInputDoi(int[], int &);
+void SumaElementelorUnuiVector(int[], int);                     //P1
+void CautareaElementuluiMinim(int[], int);                      //P2
+void OrdineaCrescatoareSauDesc(int[], int);                     //P3
+void OrdonareCrescatoareSortareLiniara(int[], int);             //P4
+void OrdonareCrescatoareSortareBubble(int[], int);              //P4
+void SumaSiDiferentaADoiVectori(int[], int, int[], int, int[]); //P5
+void InterclasareVectori(int[], int, int[], int, int[]);        //P6
+void StergereElementDePePozitiaK(int[], int &, int);            //P7
+void InserareElementPePozitiaK(int[], int &, int, int);         //P8
 int Prim(int);
-void Stergere(int[], int&, int);
-void Solutie(int[], int&);                          //P9 nu am rezolvat
-void SortareVectorElementePrime(int[], int, int);                          //P10
-void Interclasare(int[], int, int[], int, int[], int&);
+void Stergere(int[], int &, int);
+void Solutie(int[], int &);                       //P9 nu am rezolvat
+void SortareVectorElementePrime(int[], int, int); //P10
+void Interclasare(int[], int, int[], int, int[], int &);
 
-int main() {
+int main()
+{
     int x[200];
     int y[200];
     int z[200];
@@ -52,48 +53,63 @@ int main() {
     //SortareVectorElementePrime(x, 1, 10);
 }
 
-void Interclasare(int x[], int n, int y[], int m,int v[], int&k) {
+void Interclasare(int x[], int n, int y[], int m, int v[], int &k)
+{
     int i = 0, j = 0;
     k = 0;
-    while(i < n && j < m) {
-        if (x[i] == y[j]) {
+    while (i < n && j < m)
+    {
+        if (x[i] == y[j])
+        {
             v[k] = x[i];
-            i++;j++;k++;
+            i++;
+            j++;
+            k++;
         }
-        else{
-            if (x[i] < y[j]) {
+        else
+        {
+            if (x[i] < y[j])
+            {
                 v[k] = x[i];
-                i++;k++;
-
+                i++;
+                k++;
             }
-            else{
-               v[k]=y[j];
+            else
+            {
+                v[k] = y[j];
                 k++;
                 j++;
             }
         }
     }
-    while (i < n) {
+    while (i < n)
+    {
         v[k] = x[i];
-        k++;i++;
+        k++;
+        i++;
     }
-    while (j < m) {
+    while (j < m)
+    {
         v[k] = y[j];
-        k++;j++;
+        k++;
+        j++;
     }
 }
 
-int Prim(int n ) {
-    if (n==1 || n==0){
+int Prim(int n)
+{
+    if (n == 1 || n == 0)
+    {
         return 0;
     }
-    for (int i=2 ;i<=n/2;i++)
-        if (n%2==0)
+    for (int i = 2; i <= n / 2; i++)
+        if (n % 2 == 0)
             return 0;
     return 1;
 }
 
-void Stergere(int x[], int&n, int p) {
+void Stergere(int x[], int &n, int p)
+{
     for (int i = p; i < n - 1; i++)
     {
         x[i] = x[i + 1];
@@ -101,9 +117,12 @@ void Stergere(int x[], int&n, int p) {
     n--;
 }
 
-void Solutie(int x[] , int&n) {
-    for (int i = 0; i < n; i++) {
-        if (Prim(x[i])) {
+void Solutie(int x[], int &n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (Prim(x[i]))
+        {
             Stergere(x, n, i);
             i--;
         }
@@ -111,37 +130,49 @@ void Solutie(int x[] , int&n) {
     FunctieAfisare(x, n);
 }
 
-void SortareVectorElementePrime(int x[], int a, int b) {
+void SortareVectorElementePrime(int x[], int a, int b)
+{
     int contorIndex = 0;
-    for(int i = a; i <= b; i++) {
-        if(i % 2 != 0 || i != 2) {
-
+    for (int i = a; i <= b; i++)
+    {
+        if (i % 2 != 0 || i != 2)
+        {
         }
     }
 }
 
-void VerificareNumerePrime(int x[200]) {
+void VerificareNumerePrime(int x[200])
+{
     bool flag = false;
 
-    for (int i = 0; i < 200; i++) {
-        for (int j = 2; j < x[i]; j++) {
-            if (x[i] % j == 0) {
+    for (int i = 0; i < 200; i++)
+    {
+        for (int j = 2; j < x[i]; j++)
+        {
+            if (x[i] % j == 0)
+            {
                 flag = true;
                 break;
-            } else {
+            }
+            else
+            {
                 flag = false;
             }
         }
 
-        if (flag == false && x[i] > 1) {
+        if (flag == false && x[i] > 1)
+        {
             cout << x[i] << " is prime" << endl;
-        } else {
+        }
+        else
+        {
             cout << x[i] << " number is not prime" << endl;
         }
     }
 }
 
-void InserareElementPePozitiaK(int x[], int&n,int p, int nou) {
+void InserareElementPePozitiaK(int x[], int &n, int p, int nou)
+{
     for (int i = n - 1; i >= p; i--)
         x[i + 1] = x[i];
 
@@ -151,7 +182,8 @@ void InserareElementPePozitiaK(int x[], int&n,int p, int nou) {
     FunctieAfisare(x, n);
 }
 
-void StergereElementDePePozitiaK(int x[], int&n,int p) {
+void StergereElementDePePozitiaK(int x[], int &n, int p)
+{
     for (int i = p; i < n - 1; i++)
     {
         x[i] = x[i + 1];
@@ -160,53 +192,69 @@ void StergereElementDePePozitiaK(int x[], int&n,int p) {
     //FunctieAfisare(x, n);
 }
 
-void InterclasareVectori(int x[], int nX, int y[], int nY, int z[]) {
+void InterclasareVectori(int x[], int nX, int y[], int nY, int z[])
+{
     int dimensiuneZ = nX + nY;
 
-    for (int i = 0; i < nX; i++) {
+    for (int i = 0; i < nX; i++)
+    {
         z[i] = x[i];
     }
-    for (int j = nX; j < nX + nY; j++) {
+    for (int j = nX; j < nX + nY; j++)
+    {
         z[j] = y[j - nX];
     }
 
-    cout << endl << endl << "Interclasare: " << endl;
+    cout << endl
+         << endl
+         << "Interclasare: " << endl;
     OrdonareCrescatoareSortareBubble(z, dimensiuneZ);
 }
 
-void SumaSiDiferentaADoiVectori(int x[], int nX, int y[], int nY, int z[]) {
+void SumaSiDiferentaADoiVectori(int x[], int nX, int y[], int nY, int z[])
+{
     char alegere;
     cout << "Doriti sa calculati [s]uma sau [d]iferenta? : ";
     cin >> alegere;
 
-    switch(alegere) {
-        case 's':
-            if(nX == nY) {
+    switch (alegere)
+    {
+    case 's':
+        if (nX == nY)
+        {
 
-                for(int i = 0; i < nX; i++) {
-                    z[i] = x[i] + y[i];
-                }
-            } else {
-                cout << "Vectorii au dimensiuni diferite!" << endl;
+            for (int i = 0; i < nX; i++)
+            {
+                z[i] = x[i] + y[i];
             }
-            break;
-        case 'd':
-            if(nX == nY) {
+        }
+        else
+        {
+            cout << "Vectorii au dimensiuni diferite!" << endl;
+        }
+        break;
+    case 'd':
+        if (nX == nY)
+        {
 
-                for(int i = 0; i < nX; i++) {
-                    z[i] = x[i] - y[i];
-                }
-            } else {
-                cout << "Vectorii au dimensiuni diferite!" << endl;
+            for (int i = 0; i < nX; i++)
+            {
+                z[i] = x[i] - y[i];
             }
-            break;
-        default:
-            cout << "Alegere gresita.Alege s-suma sau d-diferenta!" << endl;
+        }
+        else
+        {
+            cout << "Vectorii au dimensiuni diferite!" << endl;
+        }
+        break;
+    default:
+        cout << "Alegere gresita.Alege s-suma sau d-diferenta!" << endl;
     }
     FunctieAfisare(z, nX);
 }
 
-void OrdonareCrescatoareSortareBubble(int x[], int n) {
+void OrdonareCrescatoareSortareBubble(int x[], int n)
+{
     int flag = 0;
     do
     {
@@ -219,16 +267,19 @@ void OrdonareCrescatoareSortareBubble(int x[], int n) {
                 swap(x[i], x[i + 1]);
                 flag = 1;
             }
-
         }
 
     } while (flag == 1);
 }
 
-void OrdonareCrescatoareSortareLiniara(int x[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (x[i] > x[j]) {
+void OrdonareCrescatoareSortareLiniara(int x[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (x[i] > x[j])
+            {
                 swap(x[i], x[j]);
             }
         }
@@ -236,30 +287,37 @@ void OrdonareCrescatoareSortareLiniara(int x[], int n) {
     FunctieAfisare(x, n);
 }
 
-void OrdineaCrescatoareSauDesc(int x[], int n) {
-    bool ordineC = false;// = true;
+void OrdineaCrescatoareSauDesc(int x[], int n)
+{
+    bool ordineC = false; // = true;
     bool ordineD = false;
-    for (int i = 0; i < n - 1; i++) {
+    for (int i = 0; i < n - 1; i++)
+    {
         if (x[i] < x[i + 1])
             ordineC = true;
-        else {
+        else
+        {
             ordineC = false;
             break;
         }
     }
 
-    if (ordineC == false) {
-        for (int i = 0; i < n - 1; i++) {
+    if (ordineC == false)
+    {
+        for (int i = 0; i < n - 1; i++)
+        {
             if (x[i] > x[i + 1])
                 ordineD = true;
-            else {
+            else
+            {
                 ordineD = false;
                 break;
             }
         }
     }
 
-    if (ordineC == false && ordineD == false) {
+    if (ordineC == false && ordineD == false)
+    {
         cout << " Numerele nu sunt in ordine" << endl;
     }
     else if (ordineC == true)
@@ -268,9 +326,8 @@ void OrdineaCrescatoareSauDesc(int x[], int n) {
         cout << "Numerele sunt in ordine descrescatoare" << endl;
 }
 
-
-
-void CautareaElementuluiMinim(int x[], int n) {
+void CautareaElementuluiMinim(int x[], int n)
+{
     int elementulMinim = INT_MAX;
     for (int i = 0; i < n; i++)
     {
@@ -280,33 +337,39 @@ void CautareaElementuluiMinim(int x[], int n) {
     cout << "Elementul minim din vector este: " << elementulMinim;
 }
 
-void SumaElementelorUnuiVector(int x[], int n) {
+void SumaElementelorUnuiVector(int x[], int n)
+{
     int suma = 0;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         suma = suma + x[i];
     }
     cout << "Suma elementelor din vector este: " << suma;
 }
 
-void CitireCifreInputUnu(int x[], int&n) {
+void CitireCifreInputUnu(int x[], int &n)
+{
 
     read >> n;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         read >> x[i];
     }
-
 }
 
-void CitireCifreInputDoi(int x[], int&n) {
+void CitireCifreInputDoi(int x[], int &n)
+{
     ifstream read("Input2.txt");
     read >> n;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         read >> x[i];
     }
     FunctieAfisare(x, n);
 }
 
-void FunctieAfisare(int x[], int d) {
+void FunctieAfisare(int x[], int d)
+{
     cout << endl;
     for (int i = 0; i < d; i++)
         cout << x[i] << "\t";
